3d0f1f1748952c20880c6a285814bd0dcce1844b
nik frank <nikbfrank@gmail.com>
2018-02-20 17:39:23 +0200

    step 1, promo form works

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ class App extends Component {
     name: '',
     imgSrc: '',
     eventType: '',
-
-    shoppingList: [],
   }
 
   setTab = ({ target: { value } })=> this.setState({ currentTab: value })
@@ -18,43 +16,11 @@ class App extends Component {
   setName = ({ target: { value } })=> this.setState({ name: value })
   setImgSrc = ({ target: { value } })=> this.setState({ imgSrc: value })
   setEventType = ({ target: { value } })=> this.setState({ eventType: value })
-
-  addShoppingItem = ()=>
-    this.setState(state => ({
-      shoppingList: state.shoppingList.concat({ item: '', quantity: 0 })
-    }) )
-
-  removeShoppingItem = ({ target: { value } })=>
-    this.setState(state => ({
-      shoppingList: state.shoppingList.slice(0, value*1)
-                         .concat( state.shoppingList.slice( value*1 +1 ) )
-    }) )
-  
-  setListItem = ({ target: { value, id } })=> {
-    const index = parseInt(id, 10);
-    
-    this.setState(state => ({
-      shoppingList: state.shoppingList.map( (listItem, sli)=>
-        (sli !== index) ? listItem : {...listItem, item: value}
-      )
-    }) )
-  }
-
-  setListQuantity = ({ target: { value, id } })=> {
-    const index = parseInt(id, 10);
-    
-    this.setState(state => ({
-      shoppingList: state.shoppingList.map( (listItem, sli)=>
-        (sli !== index) ? listItem : {...listItem, quantity: value}
-      )
-    }) )
-  }
   
   render() {
     const {
       tabs=[], currentTab=0,
       name, imgSrc, eventType,
-      shoppingList,
     } = this.state;
     
     return (
@@ -85,27 +51,6 @@ class App extends Component {
             
           ) : (currentTab === 1) ? (
             <div className='shopping-list'>
-              <button onClick={this.addShoppingItem}>+</button>
-              <ul>
-                {
-                  shoppingList.map( ({ item, quantity }, sli)=> (
-                    <li key={sli}>
-                      <label htmlFor={`${sli}-sli-item`}>item</label>
-                      <input id={`${sli}-sli-item`}
-                             value={item}
-                             onChange={this.setListItem}/>
-
-                      <label htmlFor={`${sli}-sli-quantity`}>quantity</label>
-                      <input id={`${sli}-sli-quantity`}
-                             value={quantity}
-                             type='number'
-                             onChange={this.setListQuantity}/>
-
-                      <button value={sli} onClick={this.removeShoppingItem}>X</button>
-                    </li>
-                  ) )
-                }
-              </ul>
             </div>
             
           ) : (currentTab === 2) ? (
