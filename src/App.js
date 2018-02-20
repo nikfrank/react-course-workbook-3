580731bcb94848bba0d03615d6d81128116f9ba2
nik frank <nikbfrank@gmail.com>
2018-02-20 17:40:41 +0200

    start without tabs

--- src/App.js
+++ src/App.js
@@ -2,44 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {
-    currentTab: 0,
-    tabs: ['Promo', 'Shopping', 'Invites']
-  }
+  state = { }
 
-  setTab = ({ target: { value } })=> this.setState({ currentTab: value })
-  
   render() {
-    const { tabs=[], currentTab=0 } = this.state;
-    
     return (
       <div className='Event'>
-        <ul className='tabs-selector'>
-          {
-            tabs.map( (tab, ti)=> (
-              <li key={ti} value={ti}
-                  onClick={this.setTab}
-                  className={(currentTab === ti) ? 'active':''}>
-                {tab}
-              </li>
-            ) )
-          }
-        </ul>
-        {
-          (currentTab === 0) ? (
-            <div className='promo-tab'>
-              
-            </div>
-          ) : (currentTab === 1) ? (
-            <div className='shopping-list'>
-              
-            </div>
-          ) : (currentTab === 2) ? (
-            <div className='invitations'>
-              
-            </div>
-          ) : null
-        }
       </div>
     );
   }
